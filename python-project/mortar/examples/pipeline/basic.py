724f221eb352cfa38d40374792bc24c254f48a03
Fritz Mahnke <fritz@mahnke.tech>
2024-10-04 15:27:27 -0700

    add basic pipeline example

---
+++ python-project/mortar/examples/pipeline/basic.py
@@ -0,0 +1,23 @@
+from importlib import resources
+
+from mortar.pipeline import *
+
+tr = resources.files('mortar.examples.data')
+iter = tr.iterdir()
+
+for it in tr.iterdir():
+    if it.name == 'hiragana_chart.png':
+        with resources.as_file(it) as path:
+            image = Image.open(path)
+
+size = (1000, 1000)
+crop = (124, 1263, 633, 1717)
+
+pipeline = Pipeline()
+pipeline.add(Crop(crop))
+pipeline.add(Gray())
+pipeline.add(Invert())
+
+output = pipeline.run(image)
+
+output.show()
