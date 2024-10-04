4eb236d716fe3f7a6e13e33e0ceaa50c0d8b8863
Fritz Mahnke <fritz@mahnke.tech>
2024-10-04 13:38:45 -0700

    accept filename to run in cli script

--- python-project/mortar/cli.py
+++ python-project/mortar/cli.py
@@ -1,6 +1,10 @@
+import sys
+
 import IPython
 from traitlets.config import Config
 # Make mortar facilities available in the IPython shell we'll embed on startup.
+
+_file = sys.argv[1]
 
 
 def main() -> int:
@@ -12,6 +16,10 @@ def main() -> int:
 
     c.InteractiveShell.confirm_exit = False
     c.TerminalIPythonApp.display_banner = False
+
+    c.InteractiveShellApp.file_to_run = _file
+    c.TerminalIPythonApp.force_interact = True
+
     IPython.start_ipython(config=c)
 
     return 0
