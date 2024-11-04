1a2c955097119e2dcc09903da74e72baf4a7ade6
Christopher Bebry <invalidsyntax@gmail.com>
2024-11-04 18:11:27 -0500

    Add example

--- python-project/mortar/examples/image/threshold.py
+++ python-project/mortar/examples/image/threshold.py
@@ -5,5 +5,7 @@ image = Image.linear_gradient('L')
 threshold = Threshold()
 
 output = threshold.run(image)
+outputDiffThreshval = threshold.run(image, 64)
 
 output.show()
+outputDiffThreshval.show()
