8f63121645945bb59bc1fe4bb28bdcc078760c65
Christopher Bebry <invalidsyntax@gmail.com>
2024-11-05 02:45:17 -0500

    Return none if wrong image mode for threshold, throwaway ret,

--- python-project/mortar/image/threshold.py
+++ python-project/mortar/image/threshold.py
@@ -15,17 +15,25 @@ from .filter import Filter
 class Threshold(Filter):
     """
     Perform thresholding on an image.
+    Must be a grayscale image (image.mode in ['1', 'L'])
     """
     name = 'Threshold'
 
-    def run(self,
-            input: PILImage,
-            threshval: float = 127,
-            maxval: float = 255,
-            invert: bool = False) -> Optional[PILImage]:
+    def __init__(self,
+                 threshval: float = 127,
+                 maxval: float = 255,
+                 invert: bool = False):
+        self.threshval = threshval
+        self.maxval = maxval
+        self.invert = invert
+
+    def run(self, input: PILImage) -> Optional[PILImage]:
         img = np.array([input.getdata()], dtype='uint8')
 
-        thresh_type = cv.THRESH_BINARY_INV if invert else cv.THRESH_BINARY
-        ret, thresh = cv.threshold(img, threshval, maxval, thresh_type)
+        if input.mode not in ['1', 'L']:
+            return None
+
+        thresh_type = cv.THRESH_BINARY_INV if self.invert else cv.THRESH_BINARY
+        _, thresh = cv.threshold(img, self.threshval, self.maxval, thresh_type)
 
         return PIL.Image.frombytes('L', input.size, bytes(thresh))
