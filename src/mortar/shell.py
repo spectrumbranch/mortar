c3f2b5cf023b6bc01676b62accbb75e616aeda1a
Fritz Mahnke <fritz@mahnke.tech>
2025-03-18 15:53:38 -0700

    feat: convert mortar script to a cli subcommand
    This change begins a unification of the various command line tools under one
    `mortar` command. They'll all be called like `mortar <subcommand>`.
    
    - Move the IPython shell code to a new `shell` module.
    - Convert the `mortar` command line script to use click. 
    - Implement a `mortar shell` subcommand, which calls the new `shell` module to
      create the shell.

---
+++ src/mortar/shell.py
@@ -0,0 +1,32 @@
+"""
+This module provides an interactive shell interface to the mortar package.
+"""
+
+import IPython
+from traitlets.config import Config
+
+
+def run(file: str | None) -> int:
+    c = Config()
+
+    # On startup, make mortar facilities available in the IPython shell.
+
+    c.InteractiveShellApp.exec_lines = [
+        'from mortar.pipeline import *',
+        'print("mortar command line interface")'
+    ]
+
+    c.InteractiveShell.confirm_exit = False
+    c.TerminalIPythonApp.display_banner = False
+
+    # If a script argument was provided, run it. Remain in the shell after the
+    # script has completed.
+
+    if file is not None:
+        c.InteractiveShellApp.file_to_run = file
+
+    c.TerminalIPythonApp.force_interact = True
+
+    IPython.start_ipython(config=c, argv=[])  # type: ignore[no-untyped-call]
+
+    return 0
