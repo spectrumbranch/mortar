5cf0f201482d691a564dd0173295bbe7dcb3ec6d
Christopher Bebry <invalidsyntax@gmail.com>
2025-09-03 01:13:24 -0400

    Add comments to help describe the process.

--- src/mortar/examples/image/iog_jp_classifier.py
+++ src/mortar/examples/image/iog_jp_classifier.py
@@ -6,10 +6,17 @@ from pprint import pprint
 from mortar.config import config
 
 
+# A tested detector fn that inputs x,y,w,h rectangle coords from each image
+# returns true if the rect is one we believe contains dialogue, else false.
+# A dialogue box in iog-jp may be in more than one position.
 def iog_jp_charity_detector_fn(x: int, _y: int, w: int, h: int) -> bool:
     if (x == 880):
         return h > 100 and w < 700
     return h > 200 and w < 1083
+
+# the purpose of this example is to make it easier to feel confident
+# throwing away junk frames of the video->screenshot collection
+# as well as another way of testing the detector function
 
 
 detector = Detector()
@@ -23,6 +30,7 @@ for file_name in os.listdir(img_directory):
     file_path = os.path.join(img_directory, file_name)
     if os.path.isfile(file_path):
         rects = detector.detect_rects(file_path, iog_jp_charity_detector_fn)
+        # default value is (0,0,0,0) when nothing detected
         representation = (0, 0, 0, 0)
         if len(rects) > 0:
             representation = rects[0]
@@ -30,9 +38,11 @@ for file_name in os.listdir(img_directory):
         if representation not in case_samples:
             case_samples[representation] = file_name
 
-
+# length of all data processed
 print(f"Data: {len(data_list)}")
 
 counter = Counter(data_list)
+# a map of the count of all cases possible [rect: int]
 pprint(f"Counter: {counter}")
+# a map of [rect: filename] which only tracks the first detected filename for a case
 pprint(f"Case Samples: {case_samples}")
